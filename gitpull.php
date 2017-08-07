91505b1d4924f62b31cda3a1ebbc3913cf3f02f4
ggRicky <ricardogg67@gmail.com>
2017-08-07 09:16:23 -0500

    2017-08-07 09:16Hrs. index.html

--- gitpull.php
+++ gitpull.php
@@ -1,1 +1,13 @@
- exec(‘git pull’);
+<?php
+echo  shell_exec('git pull');
+// echo "Hola Ricardo ....";
+
+// $salida = shell_exec('ls -lart');
+// echo "<pre>$salida</pre>";
+
+// echo shell_exec('cp index.html index.html.rgg');
+
+// echo exec("whoami"),"<br>";
+// echo exec("pwd");
+
+?>
